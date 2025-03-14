6999316dbd85ea69aa866008412109dd2cb17a60
sergio088 <ssergiosantos088@gmail.com>
2025-03-14 13:52:32 -0300

    add api

---
+++ APIs/apitempo/api.js
@@ -0,0 +1,25 @@
+const express = require ("express")
+const app = express()
+const fs = require("fs")
+const { json } = require("stream/consumers")
+
+app.use(express.json())
+
+const cidades = {
+    SaoPaulo: {temperatura:"13°", clima:"Nublado"},
+    RiodeJaneiro: {temperatura:"34°", clima:"Ensolarado"},
+    Londrina: {temperatura:"23°", clima:"chuva fraca"},
+    Fortaleza: {temperatura:"37°", clima:"Ensolarado"}
+}
+
+app.get('/clima', (req, res)=>{
+    res.json(cidades)
+})
+app.get('/clima/:cidade', (req, res) => {
+    const cidade = req.params.cidade
+    if(cidades[cidade]){
+        res.json(cidades[cidade])
+    }
+})
+
+app.listen(3000, console.log('api is running'))
